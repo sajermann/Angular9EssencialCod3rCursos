a883c0c13872a1a1ac7213e48e78ecd3c8ed9a71
Bruno Sajermann <contato@sajermann.com>
2021-04-08 11:47:21 -0300

    Video 18 Complete

--- crud/frontend/src/app/app.module.ts
+++ crud/frontend/src/app/app.module.ts
@@ -25,6 +25,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule} from '@angular/material/input';
+import { ProductReadComponent } from './components/product/product-read/product-read.component';
 
 
 @NgModule({
@@ -38,6 +39,7 @@ import { MatInputModule} from '@angular/material/input';
     RedDirective,
     ForDirective,
     ProductCreateComponent,
+    ProductReadComponent,
   ],
   imports: [
     BrowserModule,
